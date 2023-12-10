83a2d43658374e0a3d0015897fb6699277e175d7
Le Hong Quyen <hongquyen196@gmail.com>
2023-12-10 17:55:28 +0800

    update new pte 2023

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import ListItem from "./ListItem";
-import LISTENING_DICTATION from './data/LISTENING_DICTATION.json'
+import { data } from './data/LISTENING_DICTATION.json';
 
 function App() {
-  return (
-	  <div className="App">
-	  	<ListItem data={LISTENING_DICTATION}/>
-	  </div>
-  );
+	return (
+		<div className="App">
+			<ListItem data={data} />
+		</div>
+	);
 }
 
 export default App;
--- src/ListItem.tsx
+++ src/ListItem.tsx
@@ -1,19 +1,31 @@
 import React from "react";
 
 function ListItem(props: { data: any; }) {
-	const list = props.data;
+	const list = props.data.sort(function(a,b){
+		return new Date(b.createdDate) - new Date(a.createdDate);
+	  });
 	const listItems = list.map((item: any, key: any) =>
 		<div key={key}>
-			<p>{key + 1}. {item.expectAnswer}</p>
+			<p>{key + 1}. {item.answer}</p>
 			<audio id={key} controls style={{height: "20px"}} onEnded={() => handleAudioEnded(key)}>
 				<source src={item.audioLink} type="audio/mp3"/>
 			</audio>
+			<small>Date: {item.createdDate}</small>
 			<textarea rows={2} cols={50}/>
 			<hr/>
 		</div>
 	);
 	return (listItems);
 }
+
+// function downloadAudio(list: []) {
+// 	list.forEach((item: any) => {
+// 		var l: any = document.querySelector("a");
+// 		l.href = item.audioLink;
+// 		l.download = item.audioLink.split('\\').pop().split('/').pop();
+// 		l.click();
+// 	});
+// }
 
 function handleAudioEnded(key: any) {
 	let id = key + 1;
