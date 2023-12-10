d01950600140dfc0fde0723f94451052e3b9c9a5
Le Hong Quyen <hongquyen196@gmail.com>
2023-12-10 18:15:22 +0800

    Updates

---
+++ precache-manifest.640b28809cd815cc2d8c8f080aad6783.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44f7342c385a1ecfe788d45203ff8df3",
+    "url": "/pte-question-bank/index.html"
+  },
+  {
+    "revision": "4db37e3d5a4285e4fdb7",
+    "url": "/pte-question-bank/static/css/main.d3383c43.chunk.css"
+  },
+  {
+    "revision": "1f5929bab9e74fc2d234",
+    "url": "/pte-question-bank/static/js/2.a71b1702.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/pte-question-bank/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "4db37e3d5a4285e4fdb7",
+    "url": "/pte-question-bank/static/js/main.e58471d7.chunk.js"
+  },
+  {
+    "revision": "78d5e9d4869f024d04fd",
+    "url": "/pte-question-bank/static/js/runtime~main.4ebbf686.js"
+  }
+]);
